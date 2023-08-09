b85eca4ba1a2f96a8c408f5d79d6ebaa8939753b
FortisDevelopments <contactoequipofortis@gmail.com>
2023-08-08 18:09:48 -0600

    Login Rest

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const tronAPI = require('./tronAPI');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
 
 const app = express();
 const PORT = 3000;
+const SECRET_KEY = 'your-secret-key';
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'your-username',
+  password: 'your-password',
+  database: 'your-database-name'
+});
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
@@ -19,6 +29,27 @@ app.get('/transactions/:address', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
+
+app.use(bodyParser.json());
+
+// Login route
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Find user by username
+  db.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
+    if (err) throw err;
+
+    if (results.length === 0 || results[0].password !== password) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Create a JWT token
+    const token = jwt.sign({ userId: results[0].id }, SECRET_KEY, { expiresIn: '1h' });
+
+    res.json({ token });
+  });
 });
 
 app.listen(PORT, () => {
